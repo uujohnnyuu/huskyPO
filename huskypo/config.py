293d13ee4158289bb987bc92bbd17911f160792b
johnny_aws <johnny071531@gmail.com>
2024-07-10 11:42:32 +0800

    update config Swipe general actions

--- huskypo/config.py
+++ huskypo/config.py
@@ -23,3 +23,14 @@ class Appium:
     LOCALHOST = 'http://127.0.0.1'
     PORT_4723 = ':4723'
     WD_HUB = '/wd/hub'  # appium 1.0
+
+
+class Swipe:
+    UP = (0.5, 0.5, 0.5, 0.25)
+    DOWN = (0.5, 0.5, 0.5, 0.75)
+    LEFT = (0.5, 0.5, 0.25, 0.5)
+    RIGHT = (0.5, 0.5, 0.75, 0.5)
+    UPPER_LEFT = (0.5, 0.5, 0.25, 0.25)
+    UPPER_RIGHT = (0.5, 0.5, 0.75, 0.25)
+    LOWER_LEFT = (0.5, 0.5, 0.25, 0.75)
+    LOWER_RIGHT = (0.5, 0.5, 0.75, 0.75)
