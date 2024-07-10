5b571d2850b64f1abd80a23bfe209bcd11a2a4a7
johnny_aws <johnny071531@gmail.com>
2024-07-10 15:05:55 +0800

    before element

--- huskypo/config.py
+++ huskypo/config.py
@@ -7,25 +7,58 @@ from __future__ import annotations
 
 
 class Log:
+    """
+    Internal log settings.
+
+    RECORD: True means recording internal logs for huskyPO.
+    """
     RECORD = False
 
 
 class Timeout:
+    """
+    Timeout settings.
+
+    DEFAULT: The default wait time for each element, which can be directly called and modified. 
+        It will immediately affect all subsequent elements.
+    RERAISE: True means that a TimeoutException will be raised immediately when a timeout occurs; 
+        otherwise, False will be returned.
+    """
     DEFAULT = 30
     RERAISE = True
 
     @classmethod
     def reraise(cls, switch: bool | None = None) -> bool:
+        """
+        When a timeout occurs, used to determine whether to re-raise a TimeoutException or return False.
+        Typically used in wait-related methods within Element or Elements.
+        
+        Args:
+        - switch:
+            - bool: True means a TimeoutException will be raised; False means False will be returned.
+            - None: Follows the value of Timeout.RERAISE, with logic the same as bool.
+        """
         return cls.RERAISE if switch is None else switch
 
 
 class Appium:
+    """
+    General settings for Appium.
+
+    LOCALHOST: 'http://127.0.0.1'
+    PORT_4723: ':4723'
+    WD_HUB (appium 1.0): '/wd/hub'
+    """
     LOCALHOST = 'http://127.0.0.1'
     PORT_4723 = ':4723'
-    WD_HUB = '/wd/hub'  # appium 1.0
+    WD_HUB = '/wd/hub'
 
 
 class Offset:
+    """
+    Used in Page and Element to set the offset action for swipe_by and flick_by.
+    Please refer directly to the variable settings.
+    """
     UP = (0.5, 0.5, 0.5, 0.25)
     DOWN = (0.5, 0.5, 0.5, 0.75)
     LEFT = (0.5, 0.5, 0.25, 0.5)
@@ -37,4 +70,8 @@ class Offset:
 
 
 class Area:
+    """
+    Used in Page and Element to set the area action for swipe_by and flick_by.
+    Please refer directly to the variable settings.
+    """
     FULL = (0.0, 0.0, 1.0, 1.0)
