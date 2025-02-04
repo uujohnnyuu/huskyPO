c61bad4dc430550048ae29a158ef8c0a85f7732a
johnny_aws <johnny071531@gmail.com>
2025-02-04 17:45:32 +0800

    add inner log

--- huskium/element.py
+++ huskium/element.py
@@ -106,6 +106,7 @@ class Element:
         # Assign the current value to _page and clear all caches to avoid InvalidSessionIdException.
         if getattr(self, _Name._page, None) != instance:
             self._page = instance
+            self._log_info('Get new driver.')
             self._if_clear_caches()
         return self
 
@@ -119,6 +120,12 @@ class Element:
         # It’s better not to call dynamic, as it will duplicate the verification.
         self._set_data(value.by, value.value, value.index, value.timeout, value.remark, value.cache)
         self._if_clear_caches()  # dynamic element should clear caches.
+
+    def _log_info(self, msg: str | None = None):
+        logstack._info(f'Element({self.remark}): {msg}')
+
+    def _log_warning(self, msg: str | None = None):
+        logstack._warning(f'Element({self.remark}): {msg}')
 
     def dynamic(
         self,
@@ -215,6 +222,7 @@ class Element:
         if self.cache:
             for cache_name in _Name._caches:
                 vars(self).pop(cache_name, None)
+            self._log_info('Clear all caches.')
 
     def _if_force_relocate(self) -> None:
         """
@@ -378,6 +386,7 @@ class Element:
             )
             if self.cache:
                 self._present_cache = cache
+                self._log_info('Store "_present_cache" via "locator".')
             return cache
         except TimeoutException as exc:
             return self._timeout_process('present', exc, reraise)
@@ -441,6 +450,7 @@ class Element:
                 self._visible_cache = self.wait(timeout).until(
                     ecex.visibility_of_element(self._present_cache)
                 )
+                self._log_info('Store "_visible_cache" via "_present_cache".')
                 return self._visible_cache
             except ElementReferenceException:
                 cache = self.wait(timeout, EXTENDED_IGNORED_EXCEPTIONS).until(
@@ -448,6 +458,7 @@ class Element:
                 )
                 if self.cache:
                     self._visible_cache = self._present_cache = cache
+                    self._log_info('Store "_visible_cache" and "_present_cache" via "locator".')
                 return cache
         except TimeoutException as exc:
             return self._timeout_process('visible', exc, reraise)
@@ -497,6 +508,7 @@ class Element:
                 )
                 if self.cache and isinstance(cache, WebElement):
                     self._present_cache = cache
+                    self._log_info('Store "_present_cache" via "locator".')
                 return cache
         except TimeoutException as exc:
             return self._timeout_process('invisible', exc, reraise, present)
@@ -530,6 +542,7 @@ class Element:
                 self._clickable_cache = self._visible_cache = self.wait(timeout).until(
                     ecex.element_to_be_clickable(self._present_cache)
                 )
+                self._log_info('Store "_clickable_cache" and "_visible_cache" via "_present_cache".')
                 return self._clickable_cache
             except ElementReferenceException:
                 cache = self.wait(timeout, EXTENDED_IGNORED_EXCEPTIONS).until(
@@ -537,6 +550,7 @@ class Element:
                 )
                 if self.cache:
                     self._clickable_cache = self._visible_cache = self._present_cache = cache
+                    self._log_info('Store "_clickable_cache", "_visible_cache" and "_present_cache" via "locator".')
                 return cache
         except TimeoutException as exc:
             return self._timeout_process('clickable', exc, reraise)
@@ -1248,18 +1262,15 @@ class Element:
         timeout: int | float,
         max_swipe: int
     ) -> int | Literal[False]:
-        logstack._info(f'Start swiping to element {self.remark}.')
+        self._log_info('Start swiping.')
         count = 0
         while not self.is_viewable(timeout):
             if count == max_swipe:
-                logstack._warning(
-                    f'Stop swiping to element {self.remark} '
-                    f'as the maximum swipe count of {max_swipe} has been reached.'
-                )
+                self._log_warning(f'Stop swiping after {max_swipe} swipes.')
                 return False
             self.driver.swipe(*offset, duration)  # type: ignore[attr-defined]
             count += 1
-        logstack._info(f'End swiping as the element {self.remark} is now viewable.')
+        self._log_info('End swiping as the element is now viewable.')
         return count
 
     def _start_flicking_by(
@@ -1268,18 +1279,15 @@ class Element:
         timeout: int | float,
         max_swipe: int
     ) -> int | Literal[False]:
-        logstack._info(f'Start flicking to element {self.remark}.')
+        self._log_info('Start flicking.')
         count = 0
         while not self.is_viewable(timeout):
             if count == max_swipe:
-                logstack._warning(
-                    f'Stop flicking to element {self.remark} '
-                    f'as the maximum flick count of {max_swipe} has been reached.'
-                )
+                self._log_warning(f'Stop flicking after {max_swipe} flicks.')
                 return False
             self.driver.flick(*offset)  # type: ignore[attr-defined]
             count += 1
-        logstack._info(f'End flicking as the element {self.remark} is now viewable.')
+        self._log_info('End flicking as the element is now viewable.')
         return count
 
     def _start_adjusting_by(
@@ -1295,7 +1303,7 @@ class Element:
             diff = int(area - element)
             return int(math.copysign(min_distance, diff)) if abs(diff) < min_distance else diff
 
-        logstack._info(f'Start adjusting to element {self.remark}')
+        self._log_info('Start adjusting.')
 
         for i in range(1, max_adjust + 2):
 
@@ -1335,20 +1343,17 @@ class Element:
 
             # Set the end point by adjust actions.
             if adjust in adjust_actions:
-                logstack._info(f'Adjust (left, right, top, bottom): {adjust}')
+                self._log_info(f'Adjust (left, right, top, bottom): {adjust}')
                 delta_x, delta_y = adjust_actions[adjust]
                 end_x = start_x + delta_x
                 end_y = start_y + delta_y
             else:
-                logstack._info(f'End adjusting as the element {self.remark} is in area.')
+                self._log_info(f'End adjusting as the element is in area.')
                 return i
 
             # Check if the maximum number of adjustments has been reached.
             if i == max_adjust + 1:
-                logstack._warning(
-                    f'End adjusting to the element {self.remark} '
-                    f'as the maximum adjust count of {max_adjust} has been reached.'
-                )
+                self._log_warning(f'End adjusting after {max_adjust} adjustments.')
                 return False
 
             # Within the maximum adjustment attempts,
